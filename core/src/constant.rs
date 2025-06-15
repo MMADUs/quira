edb43d5af84102b19c44aaa83045344efb2d92aa
MMADUs <m.nizwa01@gmail.com>
2025-06-15 14:36:56 +0700

    feat: added boltzmann & planck mathematical constant

--- core/src/constant.rs
+++ core/src/constant.rs
@@ -19,6 +19,14 @@ use core::f64;
 
 use crate::types::Complex;
 
+/// Boltzmann constant ('k') = 1.380649 × 10^-23 J/K  
+/// Relates the average kinetic energy of particles in a gas with temperature  
+pub const BOLTZMANN: f64 = 1.380649e-23; // J/K
+
+/// Planck constant ('h') = 6.62607015 × 10^-34 J⋅s  
+/// Fundamental constant relating energy and frequency: E = h·f  
+pub const PLANCK: f64 = 6.62607015e-34; // J⋅s
+
 /// mathematical constant of pi ('π') = 3.14159265358979323846264338327950288_f64
 pub const PI: f64 = std::f64::consts::PI;
 
